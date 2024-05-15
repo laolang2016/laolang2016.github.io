3b2b399e60a12e68635d2eece85de70ea246b5be
laolang2016 <xiaodaima2016@163.com>
2024-05-15 11:41:02 +0800

    📃 docs(guide): 更新文档

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# rm -rf public
-# rm -rf docs
-# hexo d
-# mv public docs
+rm -rf public
+rm -rf docs
+hexo d
+mv public docs
 git add .
 git status
 git commit -m "📃 docs(guide): 更新文档"
